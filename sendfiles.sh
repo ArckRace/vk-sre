8615cfe8822fa52c824def8b3947d41777bff573
ArckRace <95586573+ArckRace@users.noreply.github.com>
2023-03-28 13:48:07 +0300

    Update sendfiles.sh

--- sendfiles.sh
+++ sendfiles.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 # Конфигурации
-remote_host=arck@192.168.0.178 # Адрес удаленной машины
-num_files=3 # Количество созданных файлов
-remote_dir=/home/arck/testdir # Путь директории на удаленной машине
+REMOTE_USER=arck 			# Имя пользователя удаленной машины
+REMOTE_HOST=192.168.0.178 		# Адрес удаленной машины
+NUM_FILES=3 				# Количество созданных файлов
+REMOTE_DIR=/home/$REMOTE_USER/somedir 	# Путь директории на удаленной машине
 
 # Проверка есть ли директория на удаленной машине
-if ! ssh "$remote_host" "test -d $remote_dir"; then
-  ssh "$remote_host" "mkdir $remote_dir"
-  echo "Директория $remote_dir создана на удаленной машине"
+if ! ssh "$REMOTE_USER@$REMOTE_HOST" "test -d $REMOTE_DIR"; 
+then
+  ssh "$REMOTE_USER@$REMOTE_HOST" "mkdir $REMOTE_DIR"
+  echo "Директория $REMOTE_DIR создана на удаленной машине"
 fi
 
 # Управление временем хранения файлов на другой машине
-if ssh "$remote_host" "find '$remote_dir' -maxdepth 1 -type f -name 'file?.txt' -mmin +5 -delete";
+if ssh "$REMOTE_USER@$REMOTE_HOST" "find '$REMOTE_DIR' -maxdepth 1 -type f -name 'file?.txt' -mtime +7 -delete";
 then
 	echo "Файлы удалены успешно на удаленной машине"
 else
@@ -27,14 +29,17 @@ do
 done
 
 # Изменение прав доступа на файлы перед передачей
-chmod 644 /var/log/file*.txt
+# chmod 644 /var/log/file*.txt 
 
 # Передача файлов на другую машину
-if rsync -avz /var/log/file?.txt "$remote_host:$remote_dir"; then
+if rsync -avz /var/log/file?.txt "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR"; 
+then
     echo "Файлы успешно переданы на удаленную машину"
 else
     echo "Ошибка передачи файлов на удаленную машину"
 fi
 
 # Конфигурация для crontab на выполнение скрипта:
-# 0 0 * * 0 /home/arck/bashscripts/sendfiles.sh
+# 0 0 * * * /home/arck/bashscripts/sendfiles.sh
+# Данная конфигурация будет запускать скрипт каждый день,
+# для запуска без пароля должен быть настроен ssh-ключ
